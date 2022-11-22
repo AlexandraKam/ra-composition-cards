9cc43fee6df95b049be860bdc647e59d5ec07254
Aleksandra Kameneva <kameneva_fkp@mail.ru>
2022-11-23 01:23:07 +0500

    ДЗ 1

--- intro/src/App.js
+++ intro/src/App.js
@@ -4,10 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
+  const img = "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/1.jpg";
   return (
     <>
       <Card>
-        <img src="..." className="card-img-top" alt="..."></img>
+        <img src={img} className="card-img-top" alt="..."></img>
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
